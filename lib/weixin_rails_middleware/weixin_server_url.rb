e66ed2e195c660a1870bd05ae894fc231910be42
lanrion <huaitao-deng@foxmail.com>
2014-02-19 15:46:30 +0800

    added TODO common message for weixin_server_url

--- lib/weixin_rails_middleware/weixin_server_url.rb
+++ lib/weixin_rails_middleware/weixin_server_url.rb
@@ -1,3 +1,7 @@
+# TODO: added weixin_server_url for User
+# if config +token_model+, e.g. user = User.first; user.weixin_server_url
+# if config +token_string+, e.g. weixin_server_url
+# a public url looks like: weixin_server_url
 module WeixinRailsMiddleware
   module WeixinServerUrl
     extend ActiveSupport::Concern
@@ -9,6 +13,3 @@ module WeixinRailsMiddleware
     end
   end
 end
-
-
-
