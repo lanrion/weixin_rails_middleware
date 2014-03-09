3ac8ad48f633ceca56ebcd0909035353c72bcad3
lanrion <huaitao-deng@foxmail.com>
2014-03-10 00:07:55 +0800

    clean codes

--- app/controllers/weixin_rails_middleware/weixin_controller.rb
+++ app/controllers/weixin_rails_middleware/weixin_controller.rb
@@ -17,10 +17,8 @@ module WeixinRailsMiddleware
     protected
 
       def check_weixin_params
-        if check_weixin_token_valid?
-          if !is_hexdigest?
-            render text: "Forbidden", status: 403
-          end
+        if check_weixin_token_valid? && !is_hexdigest?
+          render text: "Forbidden", status: 403
         end
       end
 
@@ -43,19 +41,10 @@ module WeixinRailsMiddleware
       def is_hexdigest?
         signature = params[:signature] || ''
         timestamp = params[:timestamp] || ''
-        nonce     = params[:nonce] || ''
+        nonce     = params[:nonce]     || ''
         current_signature = Digest::SHA1.hexdigest([current_weixin_token, timestamp, nonce].sort.join)
         return true if current_signature == signature
         false
-      end
-
-      def current_weixin_token
-        @weixin_token = params[:weixin_token]
-      end
-
-      def token_model_instance
-        token_model_instance = token_model_class.where("#{token_column}" => current_weixin_token).first
-        token_model_instance
       end
 
       ## Callback
@@ -68,6 +57,15 @@ module WeixinRailsMiddleware
       def set_weixin_message
         # Get the current_message
         @weixin_message ||= current_weixin_message
+      end
+
+      def current_weixin_token
+        @weixin_token = params[:weixin_token]
+      end
+
+      def token_model_instance
+        token_model_instance = token_model_class.where("#{token_column}" => current_weixin_token).first
+        token_model_instance
       end
 
       # return a message class with current_weixin_params
