d9aec3f1f2d782fe5e510715bd659f116fbd4092
lanrion <huaitao-deng@foxmail.com>
2014-03-09 21:08:04 +0800

    pending the weixin_server_url instance method

--- lib/weixin_rails_middleware/engine.rb
+++ lib/weixin_rails_middleware/engine.rb
@@ -1,5 +1,14 @@
 module WeixinRailsMiddleware
   class Engine < ::Rails::Engine
+    include ConfigurationHelpers
+
     isolate_namespace WeixinRailsMiddleware
+
+    config.after_initialize do
+      if token_model_class.present?
+        token_model_class.send(:include, WeixinServerUrl::InstanceMethods)
+      end
+    end
+
   end
 end
