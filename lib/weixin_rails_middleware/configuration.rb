4140b0d0b831ca5f818558c244d7af6f9094e5b9
lanrion <huaitao-deng@foxmail.com>
2014-03-09 20:21:51 +0800

    added configuration helpers

--- lib/weixin_rails_middleware/configuration.rb
+++ lib/weixin_rails_middleware/configuration.rb
@@ -9,13 +9,36 @@ module WeixinRailsMiddleware
       @token_column = DEFAULT_TOKEN_COLUMN_NAME
     end
 
+  end
+
+  module ConfigurationHelpers
+    extend ActiveSupport::Concern
+
+    def engine_path
+      @engine_path ||= WeixinRailsMiddleware.config.engine_path
+    end
+
+    def token_string
+      @token_string ||= WeixinRailsMiddleware.config.token_string.to_s
+    end
+
+    def token_column
+      @token_column ||= WeixinRailsMiddleware.config.token_column
+    end
+
+    def token_model
+      @token_model ||= WeixinRailsMiddleware.config.token_model
+    end
+
+    def is_default_engine_path?
+      engine_path == DEFAULT_ENGINE_PATH # "/"
+    end
+
     def token_model_class
       if token_model.blank?
         raise "You need to config `token_model` in 'config/initializers/weixin_rails_middleware.rb'"
       end
-      token_model_c = token_model.constantize
-      token_model_c
+      @token_model_c ||= token_model.constantize
     end
-
   end
 end
