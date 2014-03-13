007a1d6848a79fdb89adf0c96faf10b520a0c5ba
lanrion <huaitao-deng@foxmail.com>
2014-03-13 22:43:45 +0800

    clean codes

--- lib/weixin_rails_middleware/engine.rb
+++ lib/weixin_rails_middleware/engine.rb
@@ -1,15 +1,7 @@
 module WeixinRailsMiddleware
   class Engine < ::Rails::Engine
-    include ConfigurationHelpers
-
     isolate_namespace WeixinRailsMiddleware
     engine_name :weixin_engine
-
-    config.after_initialize do
-      if token_model_class.present?
-        token_model_class.send(:include, WeixinServerUrl::InstanceMethods)
-      end
-    end
 
   end
 end
