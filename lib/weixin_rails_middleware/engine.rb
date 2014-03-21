59e025b7af322eb55ea2eb62d65ed98d07a69b10
lanrion <huaitao-deng@foxmail.com>
2014-03-22 00:32:05 +0800

    added token_model_callback after initialize

--- lib/weixin_rails_middleware/engine.rb
+++ lib/weixin_rails_middleware/engine.rb
@@ -1,7 +1,25 @@
 module WeixinRailsMiddleware
   class Engine < ::Rails::Engine
+    include ConfigurationHelpers
+
     isolate_namespace WeixinRailsMiddleware
     engine_name :weixin_engine
+
+    config.after_initialize do
+      token_model_callback if token_model.present?
+    end
+
+    private
+
+      def token_model_callback
+        token_model_class.class_eval do
+          before_create do
+            # TODO: refactor
+            self.weixin_secret_key = WeiXinUniqueToken.generate(generator: :urlsafe_base64, size: 24)
+            self.weixin_token = WeiXinUniqueToken.generate if weixin_token.blank?
+          end
+        end
+      end
 
   end
 end
