a77b55bd7786d191c5993740e9823ac21065b109
lanrion <huaitao-deng@foxmail.com>
2014-03-22 00:28:34 +0800

    changed routes weixin_token param to weixin_secret_key

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 WeixinRailsMiddleware::Engine.routes.draw do
-  get  'weixin/:weixin_token', to: 'weixin#index', as: :weixin_index
-  post 'weixin/:weixin_token', to: 'weixin#reply', as: :weixin_reply
+  get  'weixin/:weixin_secret_key', to: 'weixin#index', as: :weixin_index
+  post 'weixin/:weixin_secret_key', to: 'weixin#reply', as: :weixin_reply
 end
