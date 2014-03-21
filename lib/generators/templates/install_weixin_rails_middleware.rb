9c5adb27519fc76799eda446e32da8c21a53bd7d
lanrion <huaitao-deng@foxmail.com>
2014-03-21 22:09:41 +0800

    removed token_column config

--- lib/generators/templates/install_weixin_rails_middleware.rb
+++ lib/generators/templates/install_weixin_rails_middleware.rb
@@ -3,13 +3,11 @@ WeixinRailsMiddleware.configure do |config|
 
   ## NOTE:
   ## If you config all them, it will use `token_string` default
-  ##
+
+  ## Config token_model if you SAVE public_account into database ##
   # Th first configure is fit for your weixin public_account is saved in database.
-  # +token_model+ and +token_column+ must in the same table.
   # +token_model+ The class name that to save your public_account
-  # +token_column+ You can config a column name Optional, but you must have a column `weixin_token` default.
   # config.token_model  = ""
-  # config.token_column = "weixin_token"
 
   # Or the other configure is fit for only one weixin public_account
   # If you config `token_string`, so it will directly use it
