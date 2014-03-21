a70148464da01169d01164170714d27dcb2c4422
lanrion <huaitao-deng@foxmail.com>
2014-03-22 00:30:49 +0800

    updated configurates

--- lib/generators/templates/install_weixin_rails_middleware.rb
+++ lib/generators/templates/install_weixin_rails_middleware.rb
@@ -2,19 +2,19 @@
 WeixinRailsMiddleware.configure do |config|
 
   ## NOTE:
-  ## If you config all them, it will use `token_string` default
+  ## If you config all them, it will use `weixin_token_string` default
 
-  ## Config token_model if you SAVE public_account into database ##
+  ## Config public_account_class if you SAVE public_account into database ##
   # Th first configure is fit for your weixin public_account is saved in database.
-  # +token_model+ The class name that to save your public_account
-  # config.token_model  = ""
+  # +public_account_class+ The class name that to save your public_account
+  # config.public_account_class = ""
 
   ## Here configure is for you DON'T WANT TO SAVE your public account into database ##
   # Or the other configure is fit for only one weixin public_account
-  # If you config `token_string`, so it will directly use it
-  # config.token_string = '<%= SecureRandom.hex(12) %>'
+  # If you config `weixin_token_string`, so it will directly use it
+  # config.weixin_token_string = '<%= SecureRandom.hex(12) %>'
   # using to weixin server url to validate the token can be trusted.
-  # config.weixin_secret_key = '<%= SecureRandom.hex(12) %>'
+  # config.weixin_secret_string = '<%= WeiXinUniqueToken.generate(generator: :urlsafe_base64, size: 24) %>'
 
   ## Router configure ##
   # Default is "/", and recommend you use default directly.
