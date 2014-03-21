caf1368a35e1b580e5c41e7245442c7668a8f089
lanrion <huaitao-deng@foxmail.com>
2014-03-22 00:32:20 +0800

    refactor weixin_authorize_helper

--- lib/weixin_rails_middleware/helpers/weixin_authorize_helper.rb
+++ lib/weixin_rails_middleware/helpers/weixin_authorize_helper.rb
@@ -5,20 +5,20 @@ module WeixinRailsMiddleware
     protected
 
       def check_weixin_params
-        if check_weixin_token_valid? && !is_hexdigest?
+        if is_weixin_secret_key_valid? && is_signature_invalid?
           render text: "Forbidden", status: 403
         end
       end
 
       # check the token from Weixin Service is exist in local store.
-      def check_weixin_token_valid?
-        if token_string.blank?
-          if token_model_instance.blank?
+      def is_weixin_secret_key_valid?
+        if weixin_token_string.blank?
+          if current_weixin_public_account.blank?
             render text: "Forbidden", status: 403
             return false
           end
         else
-          if current_weixin_token != token_string
+          if current_weixin_secret_key != weixin_secret_string
             render text: "Forbidden", status: 403
             return false
           end
@@ -26,22 +26,27 @@ module WeixinRailsMiddleware
         true
       end
 
-      def is_hexdigest?
-        signature = params[:signature] || ''
-        timestamp = params[:timestamp] || ''
-        nonce     = params[:nonce]     || ''
-        current_signature = Digest::SHA1.hexdigest([current_weixin_token, timestamp, nonce].sort.join)
-        return true if current_signature == signature
+      def is_signature_invalid?
+        signature   = params[:signature] || ''
+        timestamp   = params[:timestamp] || ''
+        nonce       = params[:nonce]     || ''
+        sort_params = [current_weixin_token, timestamp, nonce].sort.join
+        current_signature = Digest::SHA1.hexdigest(sort_params)
+        return true if current_signature != signature
         false
       end
 
-      def current_weixin_token
-        @weixin_token = params[:weixin_token]
+      def current_weixin_secret_key
+        @weixin_secret_key = params[:weixin_secret_key]
       end
 
-      def token_model_instance
-        token_model_instance = token_model_class.where("#{token_column}" => current_weixin_token).first
-        token_model_instance
+      def current_weixin_token
+        return weixin_token_string if weixin_token_string.present?
+        current_weixin_public_account.try(DEFAULT_TOKEN_COLUMN_NAME)
+      end
+
+      def current_weixin_public_account
+        @current_weixin_public_account = token_model_class.where("#{DEFAULT_WEIXIN_SECRET_KEY}" => current_weixin_secret_key).first
       end
 
       # return a message class with current_weixin_params
