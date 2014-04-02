1e0a10410e5f35ffea6303c24157988120c9c47f
lanrion <huaitao-deng@foxmail.com>
2014-04-02 23:23:44 +0800

    added weixin adapter

---
+++ lib/weixin_rails_middleware/adapter/weixin_adapter.rb
@@ -0,0 +1,74 @@
+# encoding: utf-8
+module WeixinRailsMiddleware
+  module Adapter
+    class WexinAdapter
+      extend ConfigurationHelpers
+
+      attr_accessor :signature, :timestamp, :nonce, :echostr
+      attr_accessor :weixin_secret_key
+
+      def initialize(weixin_params)
+        @weixin_secret_key = weixin_params[:weixin_secret_key]
+        # 以下参数为什么加空字符串默认值的原因:
+        # SB微信偶尔会再重新发一次get请求，但是不会带上signature,timestamp,nonce的参数
+        # 一个预防措施吧。
+        @signature = weixin_params[:signature] || ''
+        @timestamp = weixin_params[:timestamp] || ''
+        @nonce     = weixin_params[:nonce]     || ''
+        @echostr   = weixin_params[:echostr]   || ''
+      end
+
+      def self.init_with(weixin_params)
+        if weixin_token_string.present?
+          SinglePublicAccount.new(weixin_params)
+        else
+          MultiplePublicAccount.new(weixin_params)
+        end
+      end
+
+      def check_weixin_legality
+        return render_authorize_result(403, self.class.error_msg) if !is_signature_valid?
+        render_authorize_result(200, echostr, true)
+      end
+
+      def is_signature_valid?
+        sort_params = [current_weixin_token, timestamp, nonce].sort.join
+        current_signature = Digest::SHA1.hexdigest(sort_params)
+        return true if current_signature == signature
+        false
+      end
+
+      def current_weixin_public_account
+        raise NotImplementedError, "Subclasses must implement current_weixin_public_account method"
+      end
+
+      def current_weixin_token
+        raise NotImplementedError, "Subclasses must implement current_weixin_token method"
+      end
+
+      def is_weixin_secret_key_valid?
+        raise NotImplementedError, "Subclasses must implement is_weixin_secret_key_valid? method"
+      end
+
+      class << self
+        def error_msg
+          "#{__FILE__}:#{__LINE__}: Weixin signature NotMatch"
+        end
+      end
+
+      private
+
+        # render weixin server authorize results
+        def render_authorize_result(status=403, text=nil, valid=false)
+          text = text || error_msg
+          Rails.logger.error(text) if status != 200
+          {text: text, status: status, valid: valid}
+        end
+
+        def error_msg
+          self.class.error_msg
+        end
+
+    end
+  end
+end
