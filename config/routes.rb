674e6f4207a2630147ee9c31e025ac5724fa9f2f
lanrion <huaitao-deng@foxmail.com>
2014-04-14 20:37:07 +0800

    replace weixin_index route with weixin_server

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 WeixinRailsMiddleware::Engine.routes.draw do
-  get  'weixin/:weixin_secret_key', to: 'weixin#index', as: :weixin_index
+  get  'weixin/:weixin_secret_key', to: 'weixin#index', as: :weixin_server
   post 'weixin/:weixin_secret_key', to: 'weixin#reply', as: :weixin_reply
 end
