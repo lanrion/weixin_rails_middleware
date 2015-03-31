4c050dd9191ddca57a873e2321eb0c9df44d3d7d
lanrion <huaitao-deng@foxmail.com>
2015-03-31 21:08:53 +0800

    添加短视频消息类型

--- lib/weixin_rails_middleware/models/message.rb
+++ lib/weixin_rails_middleware/models/message.rb
@@ -39,6 +39,8 @@ module WeixinRailsMiddleware
         VoiceMessage.new(hash)
       when 'video'
         VideoMessage.new(hash)
+      when 'shortvideo'
+        ShortVideo.new(hash)
       else
         raise ArgumentError, 'Unknown Weixin Message'
       end
@@ -145,6 +147,27 @@ module WeixinRailsMiddleware
   #   <MsgId>5912593687824566343</MsgId>
   # </xml>
   class VideoMessage < Message
+
+    def MediaId
+      @source.MediaId
+    end
+
+    def ThumbMediaId
+      @source.ThumbMediaId
+    end
+  end
+
+  # <xml>
+  #   <ToUserName><![CDATA[toUser]]></ToUserName>
+  #   <FromUserName><![CDATA[fromUser]]></FromUserName>
+  #   <CreateTime>1357290913</CreateTime>
+  #   <MsgType><![CDATA[shortvideo]]></MsgType>
+  #   <MediaId><![CDATA[media_id]]></MediaId>
+  #   <ThumbMediaId><![CDATA[thumb_media_id]]></ThumbMediaId>
+  #   <MsgId>1234567890123456</MsgId>
+  # </xml>
+
+  class ShortVideo < Message
 
     def MediaId
       @source.MediaId
